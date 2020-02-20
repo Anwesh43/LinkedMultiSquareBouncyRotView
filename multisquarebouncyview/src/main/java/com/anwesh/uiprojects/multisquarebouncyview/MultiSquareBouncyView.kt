abb9a0ee464c4b7eda8ad44c8f946b05d0de8de1
Anwesh43 <anweshthecool0@gmail.com>
2020-02-20 09:55:30 +0530

    adding Renderer class

--- multisquarebouncyview/src/main/java/com/anwesh/uiprojects/multisquarebouncyview/MultiSquareBouncyView.kt
+++ multisquarebouncyview/src/main/java/com/anwesh/uiprojects/multisquarebouncyview/MultiSquareBouncyView.kt
@@ -190,4 +190,27 @@ class MultiSquareBouncyView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : MultiSquareBouncyView) {
+
+        private val animator : Animator = Animator(view)
+        private val msb : MultiBouncySquare = MultiBouncySquare(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            msb.draw(canvas, paint)
+            animator.animate {
+                msb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            msb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
