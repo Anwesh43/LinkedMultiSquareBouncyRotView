f43e99fdee5132f51fb22f425d441717cb768829
Anwesh43 <anweshthecool0@gmail.com>
2020-02-20 09:42:51 +0530

    adding MultSquareBouncyView class

--- multisquarebouncyview/src/main/java/com/anwesh/uiprojects/multisquarebouncyview/MultiSquareBouncyView.kt
+++ multisquarebouncyview/src/main/java/com/anwesh/uiprojects/multisquarebouncyview/MultiSquareBouncyView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawMRSNode(i : Int, scale : Float, paint : Paint) {
     drawMultiRotSquare(w, scale, paint)
     restore()
 }
+
+class MultiSquareBouncyView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
