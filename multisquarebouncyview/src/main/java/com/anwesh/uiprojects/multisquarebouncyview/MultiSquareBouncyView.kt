b3b1aa7e25c1ab2ded3ac500a103b4e35528936a
Anwesh43 <anweshthecool0@gmail.com>
2020-02-20 09:58:18 +0530

    adding static create method

--- multisquarebouncyview/src/main/java/com/anwesh/uiprojects/multisquarebouncyview/MultiSquareBouncyView.kt
+++ multisquarebouncyview/src/main/java/com/anwesh/uiprojects/multisquarebouncyview/MultiSquareBouncyView.kt
@@ -215,4 +215,13 @@ class MultiSquareBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : MultiSquareBouncyView {
+            val view : MultiSquareBouncyView = MultiSquareBouncyView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
